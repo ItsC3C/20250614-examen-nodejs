7bb3459eaa5500762e671cae8afe366d5e005256
Cédric <cedricvanhoorebeke@outlook.com>
2025-06-14 13:41:03 +0200

    refactor: format taskSchema definition for improved readability

--- src/models/taskModel.ts
+++ src/models/taskModel.ts
@@ -1,12 +1,16 @@
 import mongoose from "mongoose";
-import { title } from "process";
 
-const taskSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  category: String,
-  priority: String,
-  dueDate: Date,
-});
+const taskSchema = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: String,
+    dueDate: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Task = mongoose.model("Task", taskSchema);
